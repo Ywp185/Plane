fca7145ec0afd3f5c6ab9605278a319e4b269bb1
Ywp185 <954023926@qq.com>
2021-06-25 00:09:16 +0800

    planewarv1.1

--- Plane.py
+++ Plane.py
@@ -10,19 +10,74 @@ import supply
 from pygame.locals import *
 from random import *
 
+
+
+def draw_score_bombs_lifes():
+    # 绘制全屏炸弹数量
+    bomb_text = bomb_font.render("X %d" % bomb_num, True, WHITE )
+    text_rect = bomb_text.get_rect()
+    screen.blit(bomb_image, (10,height - 10 - bomb_rect.height))
+    screen.blit(bomb_text, (20 + bomb_rect.width, height - 5 - text_rect.height))
+
+    #绘制剩余生命数量
+    if life_num:
+        for i in range(life_num):
+            screen.blit(life_image, \
+                        (width - 10 - (i + 1) * life_rect.width, \
+                        height - 10 - life_rect.height))
+
+    #绘制得分
+    score_text = score_font.render("Level %s Score : %s" % (str(level), str(score)), True, WHITE)
+    screen.blit(score_text, (10,5))
+
+def draw_me():
+    screen.blit(me.image1,me.rect)
+
 pygame.init()
 pygame.mixer.init()
 
 bg_size = width, height = 480, 700
 screen = pygame.display.set_mode(bg_size)
-pygame.display.set_caption("飞机大战V1.0")
+pygame.display.set_caption("飞机大战V1.1")
 background = pygame.image.load("images/background.png").convert()
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+
+# 载入游戏音乐
+pygame.mixer.music.load("sound/game_music.ogg")
+pygame.mixer.music.set_volume(0.2)
+pygame.mixer.music.play(-1)
+
+# 统计得分
+score = 0
+score_font = pygame.font.Font("font/font.ttf", 36)
+
+# 标志是否暂停游戏
+paused = False
+
+#设置游戏难度
+level=1
+
+# 全屏炸弹
+bomb_image = pygame.image.load("images/bomb.png").convert_alpha()
+bomb_rect = bomb_image.get_rect()
+bomb_font = pygame.font.Font("font/font.ttf", 48)
+bomb_num = 3
+
+#生命数量
+life_image = pygame.image.load("images/life.png").convert_alpha()
+life_rect = life_image.get_rect()
+life_num = 3
+
+#生成我方飞机
+me = myplane.MyPlane(bg_size)
+
+clock=pygame.time.Clock()
+
+
 def main():
-    clock=pygame.time.Clock()
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -30,6 +85,9 @@ def main():
                 sys.exit()
 
         screen.blit(background,(0,0))
+
+        draw_score_bombs_lifes()
+        draw_me()
 
         pygame.display.flip()
         clock.tick(60)
